53bac2f957755c003f214e6286bc35b144f32ced
psxchi <65168787+psxchi@users.noreply.github.com>
2022-03-22 12:00:21 +0100

    implement radio

--- lib/pages/radio.dart
+++ lib/pages/radio.dart
@@ -1,22 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
-import 'musicPlayer.dart';
+import 'package:audio_session/audio_session.dart';
+import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
 
-@Deprecated("radio does not need it's own class")
-class RadioZ extends StatelessWidget {
-  const RadioZ({Key? key}) : super(key: key);
+import 'audioplay/playerSeekbar.dart';
+
+class RadioPlayer extends StatefulWidget {
+  const RadioPlayer({Key? key}) : super(key: key);
+
+  @override
+  _RadioState createState() => _RadioState();
+}
+
+class _RadioState extends State<RadioPlayer> {
+  late AudioPlayer _player;
+  final _playlist = HlsAudioSource(
+    Uri.parse(
+        "https://di-br2e5p7r.a.eurovisionflow.net/radiodvr/otp/playlist.m3u8"),
+    tag: MediaItem(
+      id: '0',
+      album: "radioZ",
+      title: "RTV RadioZ",
+      artUri: Uri.parse(
+          "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    _init();
+  }
+
+  Future<void> _init() async {
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+      //print('A stream error occurred: $e');
+    });
+    try {
+      await _player.setAudioSource(_playlist);
+    } catch (e, stackTrace) {
+      // TODO: Catch load errors: 404, invalid url ...
+      //print("Error loading playlist: $e");
+      //print(stackTrace);
+    }
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+          _player.positionStream,
+          _player.bufferedPositionStream,
+          _player.durationStream,
+          (position, bufferedPosition, duration) => PositionData(
+              position, bufferedPosition, duration ?? Duration.zero));
 
   @override
   Widget build(BuildContext context) {
-    const radioZurl = "http://mp3.rtvslo.si/raz";
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: StreamBuilder<SequenceState?>(
+                  stream: _player.sequenceStateStream,
+                  builder: (context, snapshot) {
+                    final state = snapshot.data;
+                    if (state?.sequence.isEmpty ?? true) {
+                      return const SizedBox();
+                    }
+                    final metadata = state!.currentSource!.tag as MediaItem;
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(
+                                child:
+                                    Image.network(metadata.artUri.toString())),
+                          ),
+                        ),
+                        Text(metadata.album!,
+                            style: Theme.of(context).textTheme.headline6),
+                        Text(metadata.title),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              ControlButtons(_player),
+              StreamBuilder<PositionData>(
+                stream: _positionDataStream,
+                builder: (context, snapshot) {
+                  final positionData = snapshot.data;
+                  return SeekBar(
+                    duration: positionData?.duration ?? Duration.zero,
+                    position: positionData?.position ?? Duration.zero,
+                    bufferedPosition:
+                        positionData?.bufferedPosition ?? Duration.zero,
+                    onChangeEnd: (newPosition) {
+                      _player.seek(newPosition);
+                    },
+                  );
+                },
+              ),
+              const SizedBox(height: 10.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => const MusicPlayer(
-                  url: radioZurl,
-                )));
+class ControlButtons extends StatelessWidget {
+  final AudioPlayer player;
 
-    return const Text("error displaying music player");
+  const ControlButtons(this.player, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        StreamBuilder<PlayerState>(
+          stream: player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (processingState == ProcessingState.loading ||
+                processingState == ProcessingState.buffering) {
+              return Container(
+                margin: const EdgeInsets.all(8.0),
+                width: 64.0,
+                height: 64.0,
+                child: const CircularProgressIndicator(),
+              );
+            } else if (playing != true) {
+              return IconButton(
+                icon: const Icon(Icons.play_arrow),
+                iconSize: 64.0,
+                onPressed: player.play,
+              );
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                icon: const Icon(Icons.pause),
+                iconSize: 64.0,
+                onPressed: player.pause,
+              );
+            } else {
+              return IconButton(
+                icon: const Icon(Icons.replay),
+                iconSize: 64.0,
+                onPressed: () => player.seek(Duration.zero,
+                    index: player.effectiveIndices!.first),
+              );
+            }
+          },
+        ),
+      ],
+    );
   }
 }
