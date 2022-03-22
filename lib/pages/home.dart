78624e369c97b68c67c01d3ab57a05daa7bcef4f
psxchi <65168787+psxchi@users.noreply.github.com>
2022-03-22 16:38:06 +0100

    implement api + correct layout
    "za lahko noč" is now playable
    page is scrollable

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,74 +1,110 @@
 import 'package:flutter/material.dart';
 
-import 'audioplay/musicPlayer.dart';
+import 'audioplay/music_player.dart';
+import '../audio_data.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-          body: Container(
-        margin: const EdgeInsets.only(left: 10.0, right: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Za pomiritev",
-              textScaleFactor: 2,
-            ),
-            Expanded(
-                child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                // posnetki v prvi vrsti
-                IconButton(
-                  icon: Image.asset('assets/catTable.png'),
-                  iconSize: 100,
-                  onPressed: () {
-                    playAudio(context);
-                  },
-                ),
-                TextButton(
-                  child: Image.asset('assets/catTable.png'),
-                  onPressed: () {},
-                ),
-                const Image(image: AssetImage('assets/catTable.png')),
-                const Image(image: AssetImage('assets/catTable.png')),
-              ],
-            )),
-            const Text(
-              "Pred posegom",
-              textScaleFactor: 2,
-            ),
-            Expanded(
-                child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: const [
-                // posnetki v drugi vrsti
-                Image(image: AssetImage('assets/catTable.png'))
-              ],
-            )),
-            const Text(
-              "Za lahko noč",
-              textScaleFactor: 2,
-            ),
-            Expanded(
-                child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: const [
-                // posnetki v tretji vrsti
-                Image(image: AssetImage('assets/catTable.png'))
-              ],
-            )),
-          ],
-        ),
-      ));
+  _HomeState createState() => _HomeState();
+}
 
-  // row side scrollable
+class _HomeState extends State<Home> {
+  late Future<List<AudioData>> futureAudioData;
 
-  // column ( container ( row )) x items
+  @override
+  void initState() {
+    super.initState();
+    futureAudioData = getZaLahkoNoc();
+  }
 
-  // TODO: include audio data
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        children: [
+          const Text(
+            "Za pomiritev",
+            textScaleFactor: 2,
+          ),
+          SizedBox(
+              height: height / 4,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  // posnetki v prvi vrsti
+                  IconButton(
+                    icon: Image.asset('assets/catTable.png'),
+                    iconSize: 100,
+                    onPressed: () {
+                      playAudio(context);
+                    },
+                  ),
+                  TextButton(
+                    child: Image.asset('assets/catTable.png'),
+                    onPressed: () {},
+                  ),
+                  const Image(image: AssetImage('assets/catTable.png')),
+                  const Image(image: AssetImage('assets/catTable.png')),
+                ],
+              )),
+          const Text(
+            "Pred posegom",
+            textScaleFactor: 2,
+          ),
+          SizedBox(
+              height: height / 4,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  // posnetki v drugi vrsti
+                  Image(image: AssetImage('assets/catTable.png'))
+                ],
+              )),
+          const Text(
+            "Za lahko noč",
+            textScaleFactor: 2,
+          ),
+          SizedBox(
+            height: height / 4,
+            child: FutureBuilder<List<AudioData>>(
+                future: futureAudioData,
+                builder: ((context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  } else {
+                    //return Text(snapshot.data!.title);
+                    return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.fromLTRB(5, 20, 5, 0),
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(children: [
+                            IconButton(
+                                icon: Image.network(
+                                    snapshot.data![index].imageUrl),
+                                iconSize: 100,
+                                onPressed: () {
+                                  playAudio(context);
+                                }),
+                            Text(snapshot.data![index].title),
+                          ]);
+                        });
+                  }
+                })),
+          ),
+        ],
+      ),
+    );
+  }
+
+// TODO: include audio data
   void playAudio(BuildContext context) {
     Navigator.push(
         context,
