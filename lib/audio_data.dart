72aef9195501497bbd3b67d373ef60a80cf76e00
psxchi <65168787+psxchi@users.noreply.github.com>
2022-03-29 12:49:25 +0200

    quality fix

--- lib/audio_data.dart
+++ lib/audio_data.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
+import 'package:b_air/auth/secrets.dart' as secret;
 import 'package:http/http.dart' as http;
 
 Future<List<AudioData>> getZaLahkoNoc() async {
   final response = await http.get(Uri.parse(
-      'https://api.rtvslo.si/ava/getSearch2?client_id=7c7f07d6258d2c66723451428bec4b78&pageNumber=0&pageSize=12&sort=date&order=desc&showId=54'));
+      'https://api.rtvslo.si/ava/getSearch2?client_id=${secret.storyClientId}&pageNumber=0&pageSize=12&sort=date&order=desc&showId=54'));
 
   if (response.statusCode == 200) {
     final int recNumber = getNumberOfRecordings(jsonDecode(response.body));
@@ -32,15 +33,20 @@ class AudioData {
   final String imageUrl;
   final String title;
   final String url;
+  final String? id;
 
   const AudioData(
-      {required this.imageUrl, required this.title, required this.url});
+      {required this.imageUrl,
+      required this.title,
+      required this.url,
+      this.id});
 
   factory AudioData.fromJson(Map<String, dynamic> json, int i) {
     return AudioData(
       imageUrl: json['response']['recordings'][i]['podcast_thumbnail']['md'],
       title: json['response']['recordings'][i]['title'],
       url: json['response']['recordings'][i]['link'],
+      id: json['response']['recordings'][i]['id'],
     );
   }
 }
