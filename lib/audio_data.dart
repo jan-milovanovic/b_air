01fa5bfae2457b035d082448c161b9a83da82a66
psxchi <65168787+psxchi@users.noreply.github.com>
2022-04-29 14:05:43 +0200

    add show name and description

--- lib/audio_data.dart
+++ lib/audio_data.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:pediatko/auth/secrets.dart' as secret;
 import 'package:http/http.dart' as http;
 
+import 'package:flutter/material.dart';
+import './pages/radio.dart';
+
 Future<List<AudioData>> getTrack(String showID) async {
   final response = await http.get(Uri.parse(
       'https://api.rtvslo.si/ava/getSearch2?client_id=${secret.storyClientId}&pageNumber=0&pageSize=12&sort=date&order=desc&showId=$showID'));
@@ -32,14 +35,23 @@ int getNumberOfRecordings(res) {
 class AudioData {
   final String imageUrl;
   final String title;
+  final String showName;
+  final String showDescription;
   final String url;
   final String? id;
 
   const AudioData(
       {required this.imageUrl,
       required this.title,
+      required this.showName,
+      required this.showDescription,
       required this.url,
       this.id});
+
+  void playAudio(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => RadioPlayer(audioData: this)));
+  }
 
   factory AudioData.fromJson(Map<String, dynamic> json, int i) {
     String imageUrl;
@@ -53,6 +65,8 @@ class AudioData {
     return AudioData(
       imageUrl: imageUrl,
       title: json['response']['recordings'][i]['title'],
+      showName: json['response']['recordings'][i]['showName'],
+      showDescription: json['response']['recordings'][i]['showDescription'],
       url: json['response']['recordings'][i]['link'],
       id: json['response']['recordings'][i]['id'],
     );
