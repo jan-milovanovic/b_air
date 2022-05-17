19d245b1ee7ad4d48e180ce0fe4dd051071dd701
psxchi <65168787+psxchi@users.noreply.github.com>
2022-05-17 15:05:46 +0200

    UI redesign

--- lib/pages/playlist.dart
+++ lib/pages/playlist.dart
@@ -2,22 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:pediatko/audio_data.dart';
 
 class PlaylistPage extends StatelessWidget {
-  const PlaylistPage({Key? key, required this.audioDataList}) : super(key: key);
+  const PlaylistPage(
+      {Key? key, required this.audioDataList, required this.color})
+      : super(key: key);
 
   final List<AudioData> audioDataList;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
+    final Color defaultColor = Theme.of(context).colorScheme.primary;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Pediatko'),
           centerTitle: true,
-          backgroundColor: Colors.grey.shade400,
+          backgroundColor: color,
+          elevation: 0,
         ),
-        backgroundColor: const Color.fromARGB(234, 198, 204, 255),
+        backgroundColor: color,
         body: Column(
           children: [
             const SizedBox(height: 20),
@@ -25,26 +31,40 @@ class PlaylistPage extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               child: Image(
                   image: NetworkImage(audioDataList[0].imageUrl),
-                  width: width / 2),
+                  width: width / 3),
             ),
+            const SizedBox(height: 10),
             Text(
               audioDataList[0].showName,
-              style: const TextStyle(fontSize: 20),
+              style: const TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
             ),
-            Text(audioDataList[0].showDescription),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 70,
+              width: width - 50,
+              child: SingleChildScrollView(
+                child: Text(
+                  audioDataList[0].showDescription,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
             const Spacer(
               flex: 1,
             ),
             Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 247, 249, 255),
-                border: Border.all(color: Colors.blue),
-                borderRadius: const BorderRadius.only(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 247, 249, 255),
+                borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20),
                     topRight: Radius.circular(20)),
               ),
               child: SizedBox(
-                height: height / 3,
+                height: height / 2,
                 child: ListView.builder(
                     itemCount: audioDataList.length,
                     scrollDirection: Axis.vertical,
@@ -52,17 +72,15 @@ class PlaylistPage extends StatelessWidget {
                     itemBuilder: (BuildContext context, int index) {
                       return GestureDetector(
                         onTap: () {
-                          audioDataList[index].playAudio(context);
+                          audioDataList[index].playAudio(context, color);
                         },
                         child: Column(
                           children: [
                             Row(
                               children: [
                                 const SizedBox(height: 50, width: 20),
-                                const Icon(
-                                  Icons.play_arrow_outlined,
-                                  size: 40,
-                                ),
+                                Icon(Icons.play_circle_fill_rounded,
+                                    size: 40, color: defaultColor),
                                 const SizedBox(width: 20),
                                 Expanded(
                                   child: SingleChildScrollView(
@@ -91,9 +109,10 @@ class PlaylistPage extends StatelessWidget {
   divideBetween(int listLength, int index) {
     return listLength > (index + 1)
         ? const Divider(
-            color: Colors.black,
+            color: Color.fromARGB(30, 0, 0, 0),
             indent: 20,
             endIndent: 20,
+            thickness: 3,
           )
         : const SizedBox(height: 5);
   }
