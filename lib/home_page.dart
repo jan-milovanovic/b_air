88592a7db3f9ac4da0a9d295b3bc15a1a3f56678
psxchi <65168787+psxchi@users.noreply.github.com>
2022-05-17 15:05:16 +0200

    change bottom navigation bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,6 +20,7 @@ class _HomePageState extends State<HomePage> {
             imageUrl:
                 'https://img.rtvslo.si/_up/upload/2020/04/30/65671496.jpg',
             title: 'Radio Z',
+            titleDescription: 'this is a radio..',
             showName: 'Radio Z',
             showDescription: 'radio',
             url:
@@ -31,25 +32,52 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Color defaultColor = Theme.of(context).colorScheme.primary;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PEDIATKO'),
+        title: const Text('Pediatko'),
         centerTitle: true,
-        backgroundColor: Colors.grey.shade400,
+        backgroundColor: defaultColor,
+        elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
       ),
       body: screens[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        //iconSize: 30,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.radio), label: 'Radio'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.info), label: 'Contributors'),
-        ],
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black38,
+              blurRadius: 5,
+            )
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+          child: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (index) => setState(() => currentIndex = index),
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 0,
+            //iconSize: 30,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.radio), label: 'Radio'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.info), label: 'Contributors'),
+            ],
+          ),
+        ),
       ),
     );
   }
