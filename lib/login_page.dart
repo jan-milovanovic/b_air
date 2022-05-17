0349f2c36c51cc7b43d4d204069632b17b36e47a
psxchi <65168787+psxchi@users.noreply.github.com>
2022-05-17 15:04:16 +0200

    UI redesign

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,86 +13,89 @@ class LoginPage extends StatelessWidget {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
 
+    Color defaultColor = Theme.of(context).colorScheme.primary;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: CustomPaint(
-        painter: Background(),
-        child: Container(
-          margin: const EdgeInsets.only(left: 30.0, right: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: height * 0.2),
-              welcomeText(),
-              SizedBox(height: height * 0.1),
-              inputField(inputFieldController),
-              SizedBox(height: height * 0.33),
-              Container(
-                  padding: EdgeInsets.only(left: width * 0.6),
-                  child: continueButton(inputFieldController.text, context)),
-            ],
-          ),
-        ),
-      ),
-    );
+        backgroundColor: defaultColor,
+        body: Column(
+          children: [
+            const Spacer(flex: 1),
+            const Text('Pediatko',
+                style: TextStyle(fontSize: 50, color: Colors.white)),
+            const Spacer(flex: 1),
+            Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Colors.white),
+              margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              padding: const EdgeInsets.all(20),
+              child: Column(children: [
+                const SizedBox(height: 20),
+                Icon(
+                  Icons.lock,
+                  size: 40,
+                  color: defaultColor,
+                ),
+                const SizedBox(height: 20),
+                welcomeText(),
+                const SizedBox(height: 40),
+                inputField(inputFieldController),
+                const SizedBox(height: 50),
+                continueButton(inputFieldController.text, context),
+                const SizedBox(height: 20),
+              ]),
+            ),
+            const Spacer(flex: 1),
+          ],
+        ));
   }
-}
-
-class Background extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final height = size.height;
-    final width = size.width;
-    Paint paint = Paint();
-
-    Path bottomColor = Path();
-    bottomColor.addRect(Rect.fromLTRB(0, height * 0.8, width, height));
-    //Rect.fromLTRB(left, top, right, bottom)
-    paint.color = Colors.grey.shade400;
-    canvas.drawPath(bottomColor, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
 
 RichText welcomeText() {
   return RichText(
+      textAlign: TextAlign.center,
       text: const TextSpan(
           style: TextStyle(fontSize: 24, color: Colors.black),
           children: <TextSpan>[
-        TextSpan(text: 'Dobrodošli,\n'),
-        TextSpan(
-            text: 'vse bo vredu :)',
-            style: TextStyle(fontWeight: FontWeight.bold)),
-      ]));
+            TextSpan(text: 'Dobrodošli,\n'),
+            TextSpan(
+                text: 'vse bo vredu :)',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ]));
 }
 
 TextField inputField(inputFieldController) {
   return TextField(
+    textAlign: TextAlign.center,
     controller: inputFieldController,
     keyboardType: TextInputType.number,
     inputFormatters: <TextInputFormatter>[
       FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
     ],
     decoration: const InputDecoration(
-        hintText: 'Vnesite aktivacijsko kodo', prefixIcon: Icon(Icons.key)),
+      hintText: 'Vnesite aktivacijsko kodo',
+      suffixIcon: Icon(
+        Icons.check_circle,
+        size: 40,
+        color: Color.fromARGB(50, 0, 0, 0),
+      ),
+      prefixIcon: Icon(
+        Icons.check_box_outline_blank,
+        color: Colors.white,
+      ),
+    ),
   );
 }
 
 TextButton continueButton(String passcode, BuildContext context) {
   return TextButton(
     style: TextButton.styleFrom(
-        primary: Colors.white,
-        backgroundColor: Colors.blue.shade900,
-        onSurface: Colors.black,
-        minimumSize: const Size(70.0, 70.0)),
-    child: Column(
-      children: const [
-        Icon(Icons.arrow_forward),
-        Text('Vstop'),
-      ],
+      primary: Colors.white,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      onSurface: Colors.black,
+      minimumSize: const Size(90.0, 90.0),
+      shape: const CircleBorder(),
     ),
+    child: const Icon(Icons.arrow_forward_rounded, size: 60),
     onPressed: () {
       checkValidAndProceed(passcode, context);
     },
