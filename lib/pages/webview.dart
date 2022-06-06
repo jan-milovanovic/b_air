705a9905a17f4dc39c479de9c7767efd3de1671c
psxchi <65168787+psxchi@users.noreply.github.com>
2022-06-06 23:37:03 +0200

    fetch webview url from api
    api changes include a setting for an url
    now using the url to change website by demand

--- lib/pages/webview.dart
+++ lib/pages/webview.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:pediatko/preslikave.dart';
 
+/// Third window of the homepage screen
+/// contains a body with webview custom page
+///
+/// TODO: Edges of bars are smooth, so white background leaks into the window
 class Webview extends StatefulWidget {
   const Webview({Key? key}) : super(key: key);
 
@@ -20,8 +25,26 @@ class WebviewState extends State<Webview> {
 
   @override
   Widget build(BuildContext context) {
-    return const WebView(
-      initialUrl: 'https://ars.rtvslo.si/b-air/',
+    return SizedBox(
+      //padding: EdgeInsets.all(8.0),
+      child: ClipRRect(
+        //borderRadius: BorderRadius.all(Radius.circular(20)),
+        child: FutureBuilder<Preslikave>(
+            future: preslikava,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const WebView(
+                    initialUrl: 'https://ars.rtvslo.si/b-air/');
+              }
+              if (snapshot.hasData) {
+                return WebView(initialUrl: snapshot.data!.infoPageUrl);
+              } else {
+                return Container(
+                    alignment: Alignment.center,
+                    child: const CircularProgressIndicator());
+              }
+            }),
+      ),
     );
   }
 }
