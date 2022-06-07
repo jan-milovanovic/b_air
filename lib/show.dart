2a962cd84d09645ad61b01624d98c8f98bc2a5c7
psxchi <65168787+psxchi@users.noreply.github.com>
2022-06-07 12:34:19 +0200

    add show data class

---
+++ lib/show.dart
@@ -0,0 +1,12 @@
+class Show {
+  final String showId;
+  final String title;
+  final String iconUrl;
+  final String bgColor;
+
+  Show(
+      {required this.showId,
+      required this.title,
+      required this.iconUrl,
+      required this.bgColor});
+}
