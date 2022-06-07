3c8a429c3bec599bdc57e179aef9781c001be5b9
psxchi <65168787+psxchi@users.noreply.github.com>
2022-06-07 14:55:42 +0200

    add data for radio

--- lib/preslikave.dart
+++ lib/preslikave.dart
@@ -1,18 +1,23 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:pediatko/auth/secrets.dart' as secret;
+import 'package:pediatko/dialog.dart';
 import 'show.dart';
+import 'radiodata.dart';
 
 class Preslikave {
   final List tokens;
   final String infoPageUrl;
   final List<Show> showData;
+  final RadioData radioData;
 
   Preslikave(
       {required this.tokens,
       required this.infoPageUrl,
-      required this.showData});
+      required this.showData,
+      required this.radioData});
 
   factory Preslikave.fromJson(response) {
     List<Show> firstPage = [];
@@ -25,15 +30,23 @@ class Preslikave {
           bgColor: show['bg_color'] ?? show['color']));
     }
 
+    RadioData rData = RadioData(
+      title: response['radio_live_page']['title'],
+      subtitle: response['radio_live_page']['subtile'],
+      iconUrl: response['radio_live_page']['icon_url'],
+      stream: response['radio_live_page']['stream'],
+    );
+
     return Preslikave(
       tokens: response['tokens'],
       infoPageUrl: response['settings']['info_page_url'],
       showData: firstPage,
+      radioData: rData,
     );
   }
 }
 
-Future<Preslikave> getTransformation() async {
+Future<Preslikave> getTransformation(context) async {
   try {
     final response = await http
         .get(Uri.parse(
@@ -45,7 +58,13 @@ Future<Preslikave> getTransformation() async {
     }
     throw Exception(
         'failed to load transformation data. status code = ${response.statusCode}');
+  } on TimeoutException {
+    Future.delayed(
+        const Duration(seconds: 0), noInternetConnectionDialog(context, 1));
+    throw TimeoutException('transformation timed out');
   } catch (e) {
+    Future.delayed(
+        const Duration(seconds: 0), noInternetConnectionDialog(context, 1));
     throw Exception('failed to load transformation data: $e');
   }
 }
