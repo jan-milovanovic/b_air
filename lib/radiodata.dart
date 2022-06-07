8f07c7c6e8e5c2214e69e9c199f735a9ce273ca0
psxchi <65168787+psxchi@users.noreply.github.com>
2022-06-07 14:50:36 +0200

    added radio class to fetch data in

---
+++ lib/radiodata.dart
@@ -0,0 +1,12 @@
+class RadioData {
+  final String title;
+  final String subtitle;
+  final String iconUrl;
+  final String stream;
+
+  RadioData(
+      {required this.title,
+      required this.subtitle,
+      required this.iconUrl,
+      required this.stream});
+}
