c31268abb4a97360cb5a4304e1950cb2f8f2e8b5
psxchi <65168787+psxchi@users.noreply.github.com>
2022-06-13 13:13:27 +0200

    adjust no internet connection dialog
    add willpop = false on back click
    change text

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'password_manager.dart';
 
+/// while data is still loading display a circular loading indicator
+Container loadingIndicator() {
+  return Container(
+      alignment: Alignment.center, child: const CircularProgressIndicator());
+}
+
 /// displays a dialog for a user to check their internet connection
 /// popNo -> amount of windows the dialog should close
 /// 1 = close itself
@@ -12,33 +18,36 @@ noInternetConnectionDialog(context, int popNo) {
       barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text(
-            'Preverite internetno povezavo',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20))),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                  'Prosim preverite internetno povezavo in poskusite znova.'),
-              TextButton(
-                child: const Text(
-                  'ok',
-                  style: TextStyle(fontSize: 20),
+        return WillPopScope(
+          onWillPop: () async => true,
+          child: AlertDialog(
+            title: const Text(
+              'Preverite internetno povezavo',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                    'Prosim preverite internetno povezavo in poskusite znova.'),
+                TextButton(
+                  child: const Text(
+                    'ok',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    for (int i = 0; i < popNo; i++) {
+                      Navigator.of(context).pop();
+                    }
+                    if (popNo == -1) {
+                      SystemNavigator.pop();
+                    }
+                  },
                 ),
-                onPressed: () {
-                  for (int i = 0; i < popNo; i++) {
-                    Navigator.of(context).pop();
-                  }
-                  if (popNo == -1) {
-                    SystemNavigator.pop();
-                  }
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         );
       });
@@ -49,7 +58,7 @@ noInternetConnectionDialog(context, int popNo) {
 /// moved 1 window back -> back to login screen
 AlertDialog logoutDialog(context) {
   return AlertDialog(
-    title: const Text('Ste prepričani?'),
+    title: const Text('Odjava?'),
     shape: const RoundedRectangleBorder(
       borderRadius: BorderRadius.all(
         Radius.circular(20),
