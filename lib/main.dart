1b6cf62869e11f867e4963fcec7fe436091934d1
psxchi <65168787+psxchi@users.noreply.github.com>
2022-06-14 15:59:57 +0200

    update splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,6 +10,7 @@ Future<void> main() async {
     androidNotificationChannelName: 'Audio playback',
     androidNotificationOngoing: true,
   );
+  await Future.delayed(const Duration(seconds: 1));
   runApp(const MyApp());
 }
 
@@ -29,25 +27,7 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.ptSansTextTheme(Theme.of(context).textTheme),
       ),
       //darkTheme: ThemeData.dark(),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: Lottie.asset('assets/loading.json'),
-      splashIconSize: 250,
-      duration: 2000,
-      splashTransition: SplashTransition.fadeTransition,
-      pageTransitionType: PageTransitionType.bottomToTop,
-      animationDuration: const Duration(seconds: 1),
-      //disableNavigation: true,
-      nextScreen: const LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
