4a3a3acb98fe2ba69cfa0fe001d1229ff8b78e73
psxchi <65168787+psxchi@users.noreply.github.com>
2022-09-27 13:20:50 +0200

    refactor and implement streaurl fetch
    radio live URL stream is now fetched from separate api call (rest of data stays same -> preslikave)

--- lib/radiodata.dart
+++ lib/radiodata.dart
@@ -1,12 +1,54 @@
-class RadioData {
-  final String title;
-  final String subtitle;
-  final String iconUrl;
-  final String stream;
+import 'dart:async';
+import 'dart:convert';
 
-  RadioData(
-      {required this.title,
-      required this.subtitle,
-      required this.iconUrl,
-      required this.stream});
+import 'package:http/http.dart' as http;
+import 'package:pediatko/auth/client_id.dart' as secret;
+import 'package:pediatko/dialog.dart';
+
+class RadioData {
+  final String? title;
+  final String? subtitle;
+  final String? iconUrl;
+  final String? stream;
+
+  RadioData({
+    this.title,
+    this.subtitle,
+    this.iconUrl,
+    this.stream,
+  });
+
+  factory RadioData.fromJson(response) {
+    return RadioData(
+      title: response['title'],
+      subtitle: response['subtile'],
+      iconUrl: response['icon_url'],
+      stream: response['stream'],
+    );
+  }
+
+  Future<String> fetchStreamData(context) async {
+    try {
+      final response = await http
+          .get(Uri.parse(
+              'https://api.rtvslo.si/ava/getLiveStream/ra.raz?client_id=${secret.clientId}'))
+          .timeout(const Duration(seconds: 10));
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to fetch radio data: ${response.statusCode}');
+      }
+
+      // if we'll ever need an entire radiodata again from this link
+      //return RadioData.fromJson(jsonDecode(response.body)['response']);
+      final mediaData = jsonDecode(response.body)['response']['mediaFiles'][0];
+      final streamUrl = mediaData['streamer'] + mediaData['file'];
+      return streamUrl;
+    } on TimeoutException {
+      Future.delayed(
+          const Duration(seconds: 0), noInternetConnectionDialog(context, 1));
+      throw Exception('Fetch radio data transformation timed out');
+    } catch (e) {
+      throw Exception('Failed to fetch radio data: $e');
+    }
+  }
 }
